08d08ad2c398f7213e1df68594585786931ada46
Petri Myllys <petri.myllys@reaktor.com>
2019-02-21 22:26:29 +0200

    Switch from uglifyjs to terser to not break hooks
    See https://github.com/webpack-contrib/uglifyjs-webpack-plugin/issues/374

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
+    minimizer: [new TerserPlugin()]
   }
 })
