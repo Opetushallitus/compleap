673e3ee0cd769d36f116c9b9a4acd989947fb351
Petri Myllys <petri.myllys@reaktor.com>
2019-03-11 15:26:18 +0200

    Update mock interests

--- src/state/services.js
+++ src/state/services.js
@@ -1,33 +1,20 @@
+import interests from 'mock/interests'
+import * as R from 'ramda'
 import uuid from 'uuid/v4'
+import { subtopicsLens } from 'state/helper'
 
 // TODO: implement APIs
-const getInterestSuggestions = () => Promise.resolve([{
-  topic: 'TBD1',
-  id: uuid(),
-  selected: false,
-  subtopics: [{
-    topic: 'TBDx',
-    id: uuid(),
-    selected: false
-  }, {
-    topic: 'TBDy',
-    id: uuid(),
-    selected: false
-  }]
-}, {
-  topic: 'TBD2',
-  id: uuid(),
-  selected: false,
-  subtopics: [{
-    topic: 'TBDa',
-    id: uuid(),
-    selected: false
-  }, {
-    topic: 'TBDb',
-    id: uuid(),
-    selected: false
-  }]
-}])
+const getInterestSuggestions = () => Promise.resolve(
+  interests
+    .map(interest => R.assoc('id', uuid(), interest))
+    .map(interest => R.assoc('selected', false, interest))
+    .map(interest => R.over(subtopicsLens(),
+      subtopics => subtopics
+        .map(v => R.assoc('id', uuid(), v))
+        .map(R.assoc('selected', false)),
+      interest
+    ))
+)
 
 export default {
   getInterestSuggestions
