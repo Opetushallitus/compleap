cecd65f160363b93f3ea9533ef53e0d6999455da
Petri Myllys <petri.myllys@reaktor.com>
2019-03-21 14:49:01 +0200

    Improve lander style

--- src/component/Lander.jsx
+++ src/component/Lander.jsx
@@ -5,20 +5,32 @@ import Box from 'component/generic/widget/Box'
 import LinkButton from 'component/generic/widget/LinkButton'
 
 const TextContainer = styled.section`
-  max-width: 500px;
+  max-width: 600px;
   text-align: justify;
+`
+
+const LanderButton = styled(LinkButton)`
+  padding: 1rem 2.5rem;
+  text-transform: uppercase;
+  font-weight: 500;
+`
+
+const LanderTextStyle = styled.p`
+  font-size: 1.1rem;
+  line-height: 1.8em;
+  font-weight: 300;
 `
 
 const Brief = () => (
   <TextContainer>
-    <p>{t`Etsitkö sopivaa opiskelupaikkaa? CompLeap käyttää apuna kiinnostuksiasi ja jo tehtyjä opintojasi, suositellakseen juuri sinulle sopivimpia opiskelupaikkoja.`}</p>
-    <p>{t`Kirjautumalla sisään CompLeap pystyy käyttämään Suomessa tehtyjä opintojasi apuna sopivan opiskelupaikan arvioimisessa.`}</p>
+    <LanderTextStyle>{t`Etsitkö sopivaa opiskelupaikkaa? CompLeap käyttää apuna kiinnostuksiasi ja jo tehtyjä opintojasi, suositellakseen juuri sinulle sopivimpia opiskelupaikkoja.`}</LanderTextStyle>
+    <LanderTextStyle>{t`Kirjautumalla sisään CompLeap pystyy käyttämään Suomessa tehtyjä opintojasi apuna sopivan opiskelupaikan arvioimisessa.`}</LanderTextStyle>
   </TextContainer>
 )
 
 const LoginPrompt = () => (
   <section>
-    <LinkButton href='#profile'>{t`Kirjaudu sisään`}</LinkButton>
+    <LanderButton href='#profile'>{t`Kirjaudu sisään`}</LanderButton>
   </section>
 )
 
