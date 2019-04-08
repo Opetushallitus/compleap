c0f87867919a7ad82ea6ae64ed72d8b32c0dc437
Petri Myllys <petri.myllys@reaktor.com>
2019-04-08 14:50:07 +0300

    Add mock latency also to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
@@ -7,5 +8,10 @@ module.exports = merge(common, {
     splitChunks: {
       chunks: 'all'
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.MOCK_API_LATENCY_MS': JSON.stringify(500)
+    })
+  ]
 })
