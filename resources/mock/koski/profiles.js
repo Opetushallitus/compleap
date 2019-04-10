5256cf596141d4922cd802bdb78a5213832a81a5
Petri Myllys <petri.myllys@reaktor.com>
2019-04-10 15:45:57 +0300

    Update Koski mock data

--- resources/mock/koski/profiles.js
+++ resources/mock/koski/profiles.js
@@ -1,9 +1,14 @@
 import natureAndEnvironment from 'resources/mock/koski/luontoJaYmparistoalanPerustutkinto'
+import dance from 'resources/mock/koski/tanssialanPerustutkinto'
 
 export const profileMapping = {
   '1': {
     description: 'Valmistunut ammatillisesta – luonto- ja ympäristöalan perustutkinto',
     data: natureAndEnvironment
+  },
+  '2': {
+    description: 'Valmistunut ammatillisesta – tanssialan perustutkinto',
+    data: dance
   }
 }
 
