7a2c8daada3159a06683f1f03180884cb0c1153d
Petri Myllys <petri.myllys@reaktor.com>
2019-04-29 15:33:16 +0300

    Reflect filtered number of results in group header

--- src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/RecommendationGroup.jsx
+++ src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/RecommendationGroup.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import * as R from 'ramda'
+import regions from 'resources/regions'
+import { Context } from 'state/state'
 import useTranslation from 'component/generic/hook/useTranslation'
+import useObservable from 'component/generic/hook/useObservable'
 import Expander from 'component/generic/widget/Expander'
 import SummaryHeader from './fragment/SummaryHeader'
 import Brief from './fragment/Brief'
@@ -8,12 +12,28 @@ import Description from './fragment/Description'
 import CertificateDisclaimer from './fragment/CertificateDisclaimer'
 import ApplicationOptionList from './application-option-list/ApplicationOptionList'
 
+// Note: API operates on fixed Finnish language: filter regions by Finnish names
+// TODO Consider refactoring to more robust, shared identifiers
+const regionsByName = R.compose(R.map(R.head), R.invert, R.map(langs => langs['fi']))(regions)
+const findRegionId = regionName => {
+  const id = regionsByName[regionName]
+  if (!id) {
+    console.error(`Unmatched region: cannot find region id for region name ${regionName}`)
+    return -1
+  }
+  return id
+}
+
 const RecommendationGroup = ({ title, recommendations }) => {
   const t = useTranslation()
+  const context$ = useContext(Context)
+  const locationIdWhitelist = useObservable(context$, { path: ['context', 'recommendations', 'options', 'locations'] })
+  const optionsWithRegionIds = recommendations.map(option => R.assoc('regionId', findRegionId(option.providerProvince), option))
+  const currentMatchingOptions = locationIdWhitelist.length > 0 ? optionsWithRegionIds.filter(option => locationIdWhitelist.includes(option.regionId)) : optionsWithRegionIds
 
   const Header = (
     <SummaryHeader
-      numApplicationOptions={recommendations.length}
+      numApplicationOptions={currentMatchingOptions.length}
       field={title}
       education={t`Ammatillinen tutkinto`} // TODO Replace hard-coded type with data from API when supporting multiple types
     />
@@ -24,7 +44,7 @@ const RecommendationGroup = ({ title, recommendations }) => {
       <Brief/>
       <Description/>
       <CertificateDisclaimer/>
-      <ApplicationOptionList options={recommendations}/>
+      <ApplicationOptionList options={currentMatchingOptions}/>
     </Expander>
   )
 }
--- src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/application-option-list/ApplicationOptionList.jsx
+++ src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/application-option-list/ApplicationOptionList.jsx
@@ -1,11 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import * as R from 'ramda'
-import regions from 'resources/regions'
-import { Context } from 'state/state'
 import useTranslation from 'component/generic/hook/useTranslation'
-import useObservable from 'component/generic/hook/useObservable'
 import ApplicationOptions from './fragment/ApplicationOptions'
 import ShowMoreButton from './fragment/ShowMoreButton'
 
@@ -18,38 +15,24 @@ const NoResults = styled.div`
   margin: 1rem 0;
 `
 
-const regionsByName = R.compose(R.map(R.head), R.invert, R.map(langs => langs['fi']))(regions) // TODO Note that API operates on fixed Finnish language
-const findRegionId = regionName => {
-  const id = regionsByName[regionName]
-  if (!id) {
-    console.error(`Unmatched region: cannot find region id for region name ${regionName}`)
-    return -1
-  }
-  return id
-}
-
 const ApplicationOptionList = ({ options }) => {
-  const [showAll, setShowAll] = useState(false)
   const t = useTranslation()
-  const context$ = useContext(Context)
-  const locationIdWhitelist = useObservable(context$, { path: ['context', 'recommendations', 'options', 'locations'] })
-  const optionsWithRegionIds = options.map(option => R.assoc('regionId', findRegionId(option.providerProvince), option))
-  const currentMatchingOptions = locationIdWhitelist.length > 0 ? optionsWithRegionIds.filter(option => locationIdWhitelist.includes(option.regionId)) : optionsWithRegionIds
-  const optionsToShow = showAll ? currentMatchingOptions : R.take(TRUNCATED_LIST_LENGTH, currentMatchingOptions)
+  const [showAll, setShowAll] = useState(false)
+  const optionsToShow = showAll ? options : R.take(TRUNCATED_LIST_LENGTH, options)
 
   return (
     <React.Fragment>
       <ApplicationOptions options={optionsToShow}/>
 
-      {currentMatchingOptions.length === 0 && (
+      {options.length === 0 && (
         <NoResults>{t`Ei hakukohteita nykyisillä rajauksilla`}</NoResults>
       )}
 
-      {currentMatchingOptions.length > TRUNCATED_LIST_LENGTH && (
+      {options.length > TRUNCATED_LIST_LENGTH && (
         <ShowMoreButton
           showAll={showAll}
           onClick={() => setShowAll(!showAll)}
-          numRest={currentMatchingOptions.length - TRUNCATED_LIST_LENGTH}
+          numRest={options.length - TRUNCATED_LIST_LENGTH}
         />
       )}
     </React.Fragment>
