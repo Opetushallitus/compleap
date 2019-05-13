44d237281463109da90b775908997f95f831797a
Petri Myllys <petri.myllys@reaktor.com>
2019-05-13 10:35:16 +0300

    Improve test runner reliability

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   server: {
     command: `./node_modules/.bin/http-server -p ${process.env.TEST_PORT} dist`,
-    port: parseInt(process.env.TEST_PORT)
+    port: parseInt(process.env.TEST_PORT),
+    launchTimeout: 15000
   }
 }
