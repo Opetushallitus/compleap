037519f57d5684e57a8f0543f31dd99e793c519d
ttjuntun <ttjuntun@gmail.com>
2019-05-20 07:20:46 +0000

    some fixes and changes to similarity calculation

--- model/recommendations_api.R
+++ model/recommendations_api.R
@@ -51,19 +51,23 @@ offering <- readRDS("./data/application_info.rds")
 
 ############################################################################################################
 ## API function that takes in list of study program units (or qualifications) and terms
-## Outputs list of tarjonta with meta information
-## Recommendations on institution offering level
+## Outputs list of education opportunities with meta information
 
 # uris <- c("tutkinnonosat_100125", "tutkinnonosat_100126", "tutkinnonosat_100122",
 #           "tutkinnonosat_100124", "tutkinnonosat_100123", "tutkinnonosat_100121",
 #           "tutkinnonosat_100129", "tutkinnonosat_100131", "tutkinnonosat_100130")
+# uris <- c("tutkinnonosat_100125", "tutkinnonosat_100126", "tutkinnonosat_100122")
 # uris <- c("koulutus_487141", "koulutus_381141")
-# terms <- c(1,20,34,45,58)
+# terms <- c("1,20,34,45,58")
+# uris <- c("tutkinnonosat_100439")
+# terms <- c("149,85,86,87,88")
+# terms <- c("154,125,126,127,128")
+
 
 #* @apiTitle CompLeap recommendation API
 
 #* Get similar openings in vocational education with given study units and interests
-#* @param uris The study unit (or qualification) uris to be matched with openings
+#* @param uris The study unit (or qualification) uris to be matched with education opportunities
 #* @param terms:int The interest terms ids to be matched with openings
 #* @param n:int The number of recommendations to return
 #* @param type: The type of uris in input (valid values "unit" or "qualification")
@@ -72,7 +76,7 @@ function(uris, terms, n, type = "unit") {
 
   n <- as.numeric(n) + 1
   uris <- unlist(strsplit(uris, ","))
-  terms <- unlist(terms)
+  terms <- as.integer(unlist(strsplit(terms, ",")))
 
   if(type == "unit") {
     model <- units_model
@@ -89,15 +93,18 @@ function(uris, terms, n, type = "unit") {
     interest_vecs <- interests_model[rownames(interests_model) %in% terms,]
     interest_vecs <- interest_vecs[!is.na(interest_vecs[,1]),]
   } else interest_vecs <- c()
-
+  
   # combine input vectors and calculate similarities to offering document vectors
   input_vecs <- rbind(vecs, interest_vecs)
-
+  
   # find matching education offers
-  #matches <- cosineSimilarity(offering_model, input_vecs)
   if(!is.null(input_vecs)) {
-    matches <- closest_to(offering_model, input_vecs, n)
+    #matches <- closest_to(offering_model, input_vecs, n)
+    matches <- cosineSimilarity(offering_model, input_vecs)
+    matches <- as.data.frame(cbind(rownames(matches),rowMeans(matches)))
     names(matches) <- c("offer_id","similarity")
+    matches <- matches[order(matches$similarity, decreasing = TRUE),]
+    matches <- matches[1:(n-1),]
     # match offering with metadata
     matches <- left_join(matches, offering, by = "offer_id")
   } else stop("No units or interests selected")
