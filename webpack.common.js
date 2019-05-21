224503b2a2f12654fa6ebc4b2b3a396ce3146c64
Petri Myllys <petri.myllys@reaktor.com>
2019-05-21 15:11:02 +0300

    Bump up context version

--- webpack.common.js
+++ webpack.common.js
@@ -56,7 +56,7 @@ module.exports = {
       title: 'CompLeap'
     }),
     new webpack.DefinePlugin({
-      'process.env.CONTEXT_VERSION': JSON.stringify('14'),
+      'process.env.CONTEXT_VERSION': JSON.stringify('15'),
       'process.env.DEFAULT_LANGUAGE': JSON.stringify('en'),
       'process.env.MIN_INTERESTS': JSON.stringify(3),
       'process.env.MAX_INTERESTS': JSON.stringify(10)
