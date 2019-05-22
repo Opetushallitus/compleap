f04bcf212d7224e6952b08e5f3fb4d772f97958a
Petri Myllys <petri.myllys@reaktor.com>
2019-05-22 17:04:34 +0300

    Split recommendation descriptions into paragraphs

--- src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/fragment/Description.jsx
+++ src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/fragment/Description.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import * as R from 'ramda'
 
-const Description = ({ text }) => <p>{text}</p>
+const Paragraph = styled.p`
+  margin: 0.75rem 0;
+`
+
+const splitIntoParagraphs = text => {
+  const splits = text.split(/(\.)([^. ])/)
+  return splits.reduce((res, v, i) => {
+    if (i === 0) return R.append(v, res)
+    if (v === '.') return R.adjust(res.length - 1, x => x + v, res)
+    if (v.length === 1) return R.append(v, res)
+    if (R.last(res).length === 1) return R.adjust(res.length - 1, x => x + v, res)
+    return v
+  }, [])
+}
+
+const Description = ({ text }) => splitIntoParagraphs(text).map((v, i) => <Paragraph key={i}>{v}</Paragraph>)
 
 Description.propTypes = {
   text: PropTypes.string.isRequired
