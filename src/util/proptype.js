bef318ede3cbcd58d7dd49b7dd2c8bfef1bb2154
Petri Myllys <petri.myllys@reaktor.com>
2019-05-27 09:53:46 +0300

    Fix proptype definition

--- src/util/proptype.js
+++ src/util/proptype.js
@@ -1,4 +1,7 @@
 import PropTypes from 'prop-types'
 
 export const children = PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
-export const ref = PropTypes.shape({ current: PropTypes.instanceOf(PropTypes.node) })
+export const ref = PropTypes.oneOfType([
+  PropTypes.shape({ current: PropTypes.instanceOf(PropTypes.node) }),
+  PropTypes.shape({})
+])
