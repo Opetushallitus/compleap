24f6c36c1e9aab1ec7fd6ecf075abc5022dfa309
Petri Myllys <petri.myllys@reaktor.com>
2019-06-05 14:43:53 +0300

    Format learning opportunity start date

--- src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/application-option-list/fragment/ApplicationOptions.jsx
+++ src/component/recommendations/recommendation-results/recommendation-list/recommendation-group/application-option-list/fragment/ApplicationOptions.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { Context } from 'state/state'
 import { resolveApplicationStatus } from 'util/recommendationsHelper'
 import { children } from 'util/proptype'
 import useTranslation from 'component/generic/hook/useTranslation'
+import useObservable from 'component/generic/hook/useObservable'
 import ApplicationOption from './ApplicationOption'
 
 const ApplicationOptionListStyle = styled.ul`
@@ -31,13 +33,22 @@ const withStatusFormatting = message => {
 
 const ApplicationOptions = ({ options }) => {
   const t = useTranslation()
+  const context$ = useContext(Context)
+  const lang = useObservable(context$, { path: ['context', 'user', 'language'] })
+  const locale = lang === 'fi' ? 'fi-FI' : 'en-GB'
+
   return (
     <ApplicationOptionListStyle>
       {
         options.map(option => {
           const { id, providerName, type } = option
           const { message, parameter } = resolveApplicationStatus(option)
-          const ApplicationStatus = () => <span>{t(message) + (parameter ? ` ${parameter}` : '')}</span>
+          const date = parameter && parameter.toLocaleDateString(locale, {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric'
+          })
+          const ApplicationStatus = () => <span>{t(message) + (date ? ` ${date}` : '')}</span>
           const link = `${process.env.RECOMMENDATION_CTA_BASE_URL}/${type.toLowerCase()}/${id}`
 
           return (
