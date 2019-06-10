797fd41f99ed5407d6bf462f61beec22381fdb5c
Petri Myllys <petri.myllys@reaktor.com>
2019-06-10 10:16:31 +0300

    Add alternative model endpoint to prod conf

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,8 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.MOCK_API_LATENCY_MS': JSON.stringify(500),
-      'process.env.API_ENDPOINT': JSON.stringify('https://api.compleap.testiopintopolku.fi/v2/match')
+      'process.env.API_ENDPOINT': JSON.stringify('https://api.compleap.testiopintopolku.fi/v2/match'),
+      'process.env.ALTERNATIVE_API_ENDPOINT': JSON.stringify('https://api.compleap.testiopintopolku.fi/v1/match')
     })
   ]
 })
