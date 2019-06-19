7321647f4510542d36a00e9a2d2ce937c81084bf
Petri Myllys <petri.myllys@reaktor.com>
2019-06-19 15:00:00 +0300

    Localize esco urls

--- src/component/educations/verified-educations-list/fragment/Competences.jsx
+++ src/component/educations/verified-educations-list/fragment/Competences.jsx
@@ -1,16 +1,28 @@
 import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
+import qs from 'qs'
 import { Context } from 'state/state'
 import useObservable from 'component/generic/hook/useObservable'
 import Spinner from 'component/generic/widget/Spinner'
 import Alert from 'component/generic/widget/Alert'
 import useTranslation from 'component/generic/hook/useTranslation'
 
+const getLabelMatchingLanguage = (concept, language) => {
+  switch (language) {
+    case 'fi':
+      return concept.preferredLabelFi
+    case 'en':
+    default:
+      return concept.preferredLabelEn
+  }
+}
+
 const Competences = ({ educationUri }) => {
   const t = useTranslation()
   const context$ = useContext(Context)
-  const competences = useObservable(context$, { path: ['context', 'competences', 'data', educationUri] }) || []
+  const lang = useObservable(context$, { path: ['context', 'user', 'language'] })
+  const competences = useObservable(context$, { path: ['context', 'competences', 'data', 'fromVerifiedEducation', educationUri] }) || []
   const status = useObservable(context$, { path: ['value', 'profile', 'education', 'verifiedEducationCompetences'] })
   const uniqs = R.uniqBy(R.prop('conceptUri'), competences)
 
@@ -20,13 +32,21 @@ const Competences = ({ educationUri }) => {
   return (
     <ul>
       {
-        uniqs.map(c => (
-          <li key={c.conceptUri}>
-            <a href={c.conceptUri} target='_blank' rel='noopener noreferrer'>
-              {c.preferredLabelFi}
-            </a>
-          </li>
-        ))
+        uniqs.map(c => {
+          const escoBaseUrl = `https://ec.europa.eu/esco/portal/skill`
+          const escoParameters = qs.stringify({
+            uri: c.conceptUri,
+            conceptLanguage: lang
+          })
+          const escoLink = `${escoBaseUrl}?${escoParameters}`
+          return (
+            <li key={c.conceptUri}>
+              <a href={escoLink} target='_blank' rel='noopener noreferrer'>
+                {getLabelMatchingLanguage(c,lang)}
+              </a>
+            </li>
+          )
+        })
       }
     </ul>
   )
