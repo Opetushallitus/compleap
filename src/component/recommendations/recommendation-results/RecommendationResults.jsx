a1fce8d9f7e119f473ae84c8aa0b5b45e0e6b856
Petri Myllys <petri.myllys@reaktor.com>
2019-08-21 11:58:23 +0300

    Improve alert visibility by adding whitespace

--- src/component/recommendations/recommendation-results/RecommendationResults.jsx
+++ src/component/recommendations/recommendation-results/RecommendationResults.jsx
@@ -7,11 +7,24 @@ import { RecommendationsState } from 'state/recommendationStates'
 import Alert from 'component/generic/widget/Alert'
 import useTranslation from 'component/generic/hook/useTranslation'
 import { withErrorBoundary } from 'component/generic/enhance/ErrorBoundary'
+import styled from 'styled-components'
+
+const AlertContainer = styled.div`
+  min-height: 13rem;
+  align-self: stretch;
+  margin: 1rem 0;
+`
 
 const RecommendationResults = ({ recommendations, status }) => {
   const t = useTranslation()
 
-  if (status === RecommendationsState.failure) return <Alert level='error'><p>{t`Tapahtui odottamaton virhe eikä tietoja pystytä juuri nyt näyttämään.`}</p></Alert>
+  if (status === RecommendationsState.failure) {
+    return (
+      <AlertContainer>
+        <Alert level='error'><p>{t`Tapahtui odottamaton virhe eikä tietoja pystytä juuri nyt näyttämään.`}</p></Alert>
+      </AlertContainer>
+    )
+  }
 
   return (
     <React.Fragment>
